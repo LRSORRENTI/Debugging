e1a5ea04acc2e745452513835985990a291f629e
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-24 18:16:16 -0500

    http-server is not recognizing my index.js, but live-server is....

---
+++ 02-Concepts/debug/index.js
@@ -0,0 +1,8 @@
+const txtTrollName = document.getElementById("txtTrollName");
+const txtTrollMsg = document.getElementById("txtTrollMsg");
+const btn = document.getElementById('btnNewTrollMessage')
+console.log(123)
+console.log(btn)
+btn.addEventListener( 'click',  () => {
+    console.log(1)
+})
